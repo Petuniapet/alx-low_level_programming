58963aff70fb717398554360bd955bade89d5617
Petunia Stemere <petuniastemere@gmail.com>
2023-06-15 05:41:22 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - A program that print the line using a printf function
+ * *
+ * Return: 0 (success)
+ **/
+int main(void)
+{
+	print("with proper grammar, but the outcome is a piece of art\n");
+	return (0);
+}
