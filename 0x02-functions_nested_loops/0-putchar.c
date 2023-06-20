ca8d817ae112dd327629e9ab86e6050e132156ee
Petunia Stemere <petuniastemere@gmail.com>
2023-06-20 08:00:18 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,18 +2,21 @@
 #include<stdio.h>
 /**
  * main - print _putchar
- * Return: 0 (Success)
+ * is written by mbah
+ * Return: 0
  */
+
 int main(void)
 {
-	 putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
+
