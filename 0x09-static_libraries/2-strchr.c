7ab75037d2fd3e91494e1417edf813fba809898e
Petunia Stemere <petuniastemere@gmail.com>
2023-07-10 06:15:34 -0700

    C - Static libraries

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strchr - Entry point
+ * @s: input
+ * @c: input
+ * Return: Always 0 (Success)
+ */
+char *_strchr(char *s, char c)
+{
+	int o = 0;
+
+	for (; s[o] >= '\0'; o++)
+	{
+		if (s[o] == c)
+			return (&s[o]);
+	}
+	return (0);
+}
